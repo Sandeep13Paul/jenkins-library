ce3f0996c4143a6de0c51cb904bdac676cc08d28
Sandeep Paul <123166400+Sandeep13Paul@users.noreply.github.com>
2025-09-08 09:10:30 +0000

    added changes

--- vars/geetaCi.groovy
+++ vars/geetaCi.groovy
@@ -6,8 +6,8 @@ def call(Map config) {
             DOCKERHUB_CREDENTIALS = credentials("${config.dockerCreds}")
             DOCKERHUB_REPO = "${config.dockerRepo}"
             APP_VERSION = "${config.appVersion}"
-            DOCKER_CMD = "/usr/local/bin/docker"
-            KUBECTL_CMD = "/usr/local/bin/kubectl"
+            // DOCKER_CMD = "/usr/local/bin/docker"
+            // KUBECTL_CMD = "/usr/local/bin/kubectl"
         }
         
         stages {
